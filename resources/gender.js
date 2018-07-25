5870ad78a799dd6894a85d37c1a8f73a244f0867
Mosely <djhiggs1@gmail.com>
2018-07-24 22:34:52 -0500

    Trying Again

--- resources/gender.js
+++ resources/gender.js
@@ -1,20 +1,24 @@
 const jagql = require("@jagql/framework");
 const RelationalDbStore = require("@jagql/store-sequelize");
 
+let theTable = new RelationalDbStore({
+  dialect: process.env.DB_DRIVER,
+  dialectOptions: {
+    supportBigNumbers: true
+  },
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  logging: console.log
+});
+
+theTable.populate();
+
 jagql.define({
   resource: "genders",
-  handlers: new RelationalDbStore({
-    dialect: process.env.DB_DRIVER,
-    dialectOptions: {
-      supportBigNumbers: true
-    },
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    logging: console.log
-  }).populate(),
+  handlers: theTable,
   attributes: {
     name: jagql.Joi.string(),
     people: jagql.Joi.belongsToMany({resource: "people", as: "gender"}),
