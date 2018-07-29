97d6081ed722f380139b497ed90ff1eec57982df
Mosely <djhiggs1@gmail.com>
2018-07-28 20:21:17 -0500

    Typo

--- resources/person.js
+++ resources/person.js
@@ -8,7 +8,7 @@ module.exports = function (jagql, sql) {
       middleName: jagql.Joi.string(),
       dateOfBirth: jagql.Joi.date(),
       age: jagql.Joi.number(),
-      //gender: jagql.Joi.one('genders'),
+      gender: jagql.Joi.one('genders'),
       updatedBy: jagql.Joi.number().default(1)
     }
   });
