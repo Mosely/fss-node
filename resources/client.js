d16e0d1d5de0232db17e22307dd97064c6fce545
Mosely <djhiggs1@gmail.com>
2018-07-28 19:33:09 -0500

    Even More Testing

--- resources/client.js
+++ resources/client.js
@@ -1,20 +1,20 @@
 const jagql = require("@jagql/framework");
 const RelationalDbStore = require("@jagql/store-sequelize");
-
+let sql = new RelationalDbStore({
+  dialect: process.env.DB_DRIVER,
+  dialectOptions: {
+    supportBigNumbers: true
+  },
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  logging: console.log
+});
 jagql.define({
   resource: "clients",
-  handlers: new RelationalDbStore({
-    dialect: process.env.DB_DRIVER,
-    dialectOptions: {
-      supportBigNumbers: true
-    },
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    logging: console.log
-  }),
+  handlers: sql,
   attributes: {
     socialSecurityNumber: jagql.Joi.number(),
     placeOfEmployment: jagql.Joi.string(),
@@ -22,6 +22,10 @@ jagql.define({
     hasFamilyWhoIsServiceMemberOrVeteran: jagql.Joi.boolean(),
     isReferredByVeteranResourceCenter: jagql.Joi.boolean(),
     referral: jagql.Joi.string(),
-    updatedBy: jagql.Joi.number().default(1)
+    updatedBy: jagql.Joi.number().default(1),
+    ethnicities: jagql.joi.Many("ethnicities")
   }
+});
+sql.populate({force: false}, () => {
+  //tables dropped and created
 });
--- resources/ethnicity.js
+++ resources/ethnicity.js
@@ -1,22 +1,26 @@
 const jagql = require("@jagql/framework");
 const RelationalDbStore = require("@jagql/store-sequelize");
-
+let sql = new RelationalDbStore({
+  dialect: process.env.DB_DRIVER,
+  dialectOptions: {
+    supportBigNumbers: true
+  },
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  logging: console.log
+});
 jagql.define({
   resource: "ethnicities",
-  handlers: new RelationalDbStore({
-    dialect: process.env.DB_DRIVER,
-    dialectOptions: {
-      supportBigNumbers: true
-    },
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    logging: console.log
-  }),
+  handlers: sql,
   attributes: {
     name: jagql.Joi.string(),
-    updatedBy: jagql.Joi.number().default(1)
+    updatedBy: jagql.Joi.number().default(1),
+    clients: jagql.joi.belongsToMany({resource:"clients"})
   }
+});
+sql.populate({force: false}, () => {
+  //tables dropped and created
 });
